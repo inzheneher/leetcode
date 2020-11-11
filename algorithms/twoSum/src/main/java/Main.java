70d1b00add9f17907d75d45b865bf82ea035f59a
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-11-11 15:32:22 -0500

    added printing output array

---
+++ algorithms/twoSum/src/main/java/Main.java
@@ -0,0 +1,19 @@
+/**
+ * inzheneher created on 11/11/2020 inside the package - PACKAGE_NAME
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] a = solution.twoSum(new int[]{2, 3, 6, 9, 5}, 15);
+
+        StringBuilder output = new StringBuilder("[");
+        for (int i = 0; i < a.length; i++) {
+            if (i == a.length - 1) {
+                output.append(a[i]).append("]");
+                break;
+            }
+            output.append(a[i]).append(", ");
+        }
+        System.out.println(output.toString());
+    }
+}

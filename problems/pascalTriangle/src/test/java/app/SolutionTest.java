61d30a5676405488a65022881b685f56ee6c6229
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-02-17 12:54:01 -0500

    Add tests for 1, 2 and 5 rows of triangele.  Changes to be committed: 	modified:   problems/pascalTriangle/src/test/java/app/SolutionTest.java

--- problems/pascalTriangle/src/test/java/app/SolutionTest.java
+++ problems/pascalTriangle/src/test/java/app/SolutionTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,14 +16,23 @@ import static org.junit.Assert.*;
 public class SolutionTest {
 
     private Solution solution;
+    private List<List<Integer>> testArrayWithOneRow;
+    private List<List<Integer>> testArrayWithTwoRows;
+    private List<List<Integer>> testArrayWithFiveRows;
 
     @Before
     public void init() {
         solution = new Solution();
+        testArrayWithOneRow = Collections.singletonList(Collections.singletonList(1));
+        testArrayWithTwoRows = Arrays.asList(Collections.singletonList(1), Arrays.asList(1, 1));
+        testArrayWithFiveRows = Arrays.asList(
+                Collections.singletonList(1), Arrays.asList(1, 1), Arrays.asList(1, 2, 1), Arrays.asList(1, 3, 3, 1), Arrays.asList(1, 4, 6, 4, 1));
     }
 
     @Test
     public void generate() {
-        assertEquals(new ArrayList<List<Integer>>(), solution.generate(6));
+        assertEquals(testArrayWithOneRow, solution.generate(1));
+        assertEquals(testArrayWithTwoRows, solution.generate(2));
+        assertEquals(testArrayWithFiveRows, solution.generate(5));
     }
 }

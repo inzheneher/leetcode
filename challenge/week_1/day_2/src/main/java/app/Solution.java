6d084c097549882d9f6a6342fb643f78cc7ca329
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-02 17:29:06 -0500

    Created template for Day_2 Solution.

---
+++ challenge/week_1/day_2/src/main/java/app/Solution.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.util.Arrays;
+
+/**
+ * inzheneher created on 02/03/2021 inside the package - app
+ */
+public class Solution {
+    public int[] findErrorNums(int[] nums) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.findErrorNums(new int[]{})));
+    }
+}

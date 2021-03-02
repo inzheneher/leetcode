31b6ee698fb73f1981445690ec427721ca7f2825
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-02 16:59:19 -0500

    Solution on Set

--- challenge/week_1_day_1/src/main/java/app/Solution.java
+++ challenge/week_1_day_1/src/main/java/app/Solution.java
@@ -1,6 +1,5 @@
 package app;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,14 +8,13 @@ import java.util.Set;
  */
 public class Solution {
     public int distributeCandies(int[] candyType) {
-        Set<Integer> unicCandys = new HashSet<>(Arrays.asList(Arrays.stream(candyType).boxed().toArray(Integer[]::new)));
-        int arrSize = candyType.length;
-        int setSize = unicCandys.size();
-        return Math.min(setSize, arrSize/2);
+        Set<Integer> setFromArray = new HashSet<>();
+        for (int item : candyType) setFromArray.add(item);
+        return Math.min(setFromArray.size(), candyType.length / 2);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.distributeCandies(new int[]{6,6,6,6}));
+        System.out.println(solution.distributeCandies(new int[]{1, 1, 1, 2, 2, 3, 4, 5, 5, 6}));
     }
 }

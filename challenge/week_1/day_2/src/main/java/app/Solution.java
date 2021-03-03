0c75581055c277d8d0f823699e08903d4d5da5c2
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-03 01:25:57 -0500

    Set mistmatch solved faster than 100% of submissions.

--- challenge/week_1/day_2/src/main/java/app/Solution.java
+++ challenge/week_1/day_2/src/main/java/app/Solution.java
@@ -7,11 +7,23 @@ import java.util.Arrays;
  */
 public class Solution {
     public int[] findErrorNums(int[] nums) {
+        int numSize = nums.length;
+        int duplicateNumber = -1;
+        boolean[] presence = new boolean[numSize + 1];
+        for (int num : nums) {
+            if (!presence[num]) presence[num] = true;
+            else duplicateNumber = num;
+        }
+        for (int i = 1; i < presence.length; i++) if (!presence[i]) return new int[]{duplicateNumber, i};
         return null;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.findErrorNums(new int[]{})));
+        System.out.printf("[1, 1] -> %s%n", Arrays.toString(solution.findErrorNums(new int[]{1, 1})));
+        System.out.printf("[2, 2] -> %s%n", Arrays.toString(solution.findErrorNums(new int[]{2, 2})));
+        System.out.printf("[3, 2, 2] -> %s%n", Arrays.toString(solution.findErrorNums(new int[]{3, 2, 2})));
+        System.out.printf("[1, 2, 2, 4] -> %s%n", Arrays.toString(solution.findErrorNums(new int[]{1, 2, 2, 4})));
+        System.out.printf("[1, 5, 3, 4, 5, 6, 7, 8] -> %s%n", Arrays.toString(solution.findErrorNums(new int[]{1, 5, 3, 4, 5, 6, 7, 8})));
     }
 }

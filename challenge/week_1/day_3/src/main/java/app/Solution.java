dbe7dca2d88ca3aac1173d05c41d5a725da61873
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-03 10:17:01 -0500

    Added task explanation as a comment  Changes to be committed: 	modified:   challenge/week_1/day_3/src/main/java/app/Solution.java

--- challenge/week_1/day_3/src/main/java/app/Solution.java
+++ challenge/week_1/day_3/src/main/java/app/Solution.java
@@ -2,6 +2,11 @@ package app;
 
 /**
  * inzheneher created on 03/03/2021 inside the package - app
+ *
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ *
+ * Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
  */
 public class Solution {
     public int missingNumber(int[] nums) {

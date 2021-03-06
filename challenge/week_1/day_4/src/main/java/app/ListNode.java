d629a9e6252ae4ebd2f45b51fc2d32102582ca1b
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-05 23:16:23 -0500

    Intersection of Two Linked Lists solved.  Changes to be committed: 	new file:   challenge/week_1/day_4/pom.xml 	new file:   challenge/week_1/day_4/src/main/java/app/ListNode.java 	new file:   challenge/week_1/day_4/src/main/java/app/Solution.java 	modified:   challenge/week_1/pom.xml

---
+++ challenge/week_1/day_4/src/main/java/app/ListNode.java
@@ -0,0 +1,14 @@
+package app;
+
+/**
+ * inzheneher created on 04/03/2021 inside the package - app
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ challenge/week_1/day_4/src/main/java/app/Solution.java
@@ -0,0 +1,41 @@
+package app;
+
+/**
+ * inzheneher created on 04/03/2021 inside the package - app
+ *
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode head1 = headA;
+        ListNode head2 = headB;
+        while (head1 != head2) {
+            if (head1 == null) head1 = headB;
+            else head1 = head1.next;
+            if (head2 == null) head2 = headA;
+            else head2 = head2.next;
+        }
+        return head1;
+    }
+
+    //listA = [4,1,8,4,5], listB = [5,6,1,8,4,5]
+    private ListNode getHeadOfLinkedListFromArray(int[] array) {
+        int arrL = array.length;
+        ListNode previousNode = new ListNode(array[arrL - 1]);
+        ListNode currentNode = null;
+        for (int i = arrL - 2; i >= 0; i--) {
+            currentNode = new ListNode(array[i]);
+            currentNode.next = previousNode;
+            if (i > 0) previousNode = currentNode;
+        }
+        return currentNode;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head1 = solution.getHeadOfLinkedListFromArray(new int[]{4,1,8,4,5});
+        ListNode head2 = solution.getHeadOfLinkedListFromArray(new int[]{5,6,1,8,4,5});
+        System.out.printf("Intersection number is: %s%n", solution.getIntersectionNode(head1, head2).val);
+    }
+}

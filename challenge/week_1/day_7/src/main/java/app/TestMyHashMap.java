1766d27bf480a84cb50255026ee0d1b5587e0592
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-03-12 11:51:24 -0500

    Added HashMap solution  Changes to be committed: 	modified:   challenge/pom.xml 	new file:   challenge/week_1/day_6/pom.xml 	new file:   challenge/week_1/day_7/pom.xml 	new file:   challenge/week_1/day_7/src/main/java/app/MyHashMap.java 	new file:   challenge/week_1/day_7/src/main/java/app/TestMyHashMap.java 	modified:   challenge/week_1/pom.xml 	new file:   challenge/week_2/day_8/pom.xml 	new file:   challenge/week_2/pom.xml

---
+++ challenge/week_1/day_7/src/main/java/app/TestMyHashMap.java
@@ -0,0 +1,32 @@
+package app;
+
+/**
+ * inzheneher created on 08/03/2021 inside the package - app
+ */
+public class TestMyHashMap {
+    public static void main(String[] args) {
+        MyHashMap obj = new MyHashMap();
+        obj.remove(14);
+        obj.get(4);
+        obj.put(7, 3);
+        obj.put(11, 1);
+        obj.put(12, 1);
+        obj.get(7);
+        obj.put(1, 19);
+        obj.put(0, 3);
+        obj.put(1, 8);
+        obj.put(2, 6);
+//        int val2 = obj.get(3);
+//        obj.put(1, 1);
+//        obj.put(2, 2);
+//        int val1 = obj.get(1);
+//        obj.get(1);
+//        obj.get(3);
+//        obj.put(2, 1);
+//        obj.get(2);
+//        obj.remove(2);
+//        obj.get(2);
+//        int val3 = obj.get(1);
+//        int val4 = obj.get(2);
+    }
+}
